9bec4e0451d4003877251334fe9e895c808b29d6
peris-kagunya <periskagunya5@gmail.com>
2024-06-14 13:40:49 +0300

    initial commit

---
+++ resources/views/mobile/welcome.blade.php
@@ -0,0 +1,11 @@
+<x-mobile-layout>
+
+    <nav class="h-20 bg-orange-600 px-3">
+        <div class="flex items-center text-white justify-between">
+            <h1 class="font-bold">Sch. Bus</h1>
+
+            <span class="text-white">Login</span>
+        </div>
+    </nav>
+
+</x-mobile-layout>
