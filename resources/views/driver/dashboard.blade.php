39d7b49dc4cf5e2c88252a87b12434a545ab908c
titustum <tituskiptanuitum@gmail.com>
2024-06-18 12:35:34 +0300

    smarter additions

--- resources/views/driver/dashboard.blade.php
+++ resources/views/driver/dashboard.blade.php
@@ -1,12 +1,106 @@
 <x-mobile-layout header="Driver">
  
 
-    <div class="h-screen w-full mt-8 flex flex-col p-4">
+   <div class="h-screen w-full my-16 flex flex-col p-4">
+ 
+            <div class="text-orange-600 text-xl text-center uppercase font-['Righteous']">
+                DRIVER DASHBOARD
+            </div>
+    
+            <form class="mt-3 p-2 shadow rounded-md border">
 
-        <h1 class="text-2xl font-bold text-center uppercase">Driver Dashboard</h1>
+                <h1 class="font-bold text-center">Create A Route</h1>
+
+                @csrf
+                
+                <!-- destination -->
+                <div class="mt-4">
+                    <x-input-label for="usertype" :value="__('Select origin')" />
+                    <select id="usertype" class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm block mt-1 w-full" name="usertype" :value="old('usertype')" required />
+                        <option>Mombasa</option>
+                        <option>Nairobi</option>
+                        <option>Bungoma</option>
+                        <option>Kisumu</option>
+                        <option>Eldoret</option>
+                        <option>Nyeri</option>
+                    </select>
+                    <x-input-error :messages="$errors->get('usertype')" class="mt-2" />
+                </div>
+    
+                <!-- destination -->
+                <div class="mt-4">
+                    <x-input-label for="usertype" :value="__('Select destination')" />
+                    <select id="usertype" class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm block mt-1 w-full" name="usertype" :value="old('usertype')" required />
+                        <option>Nairobi</option>
+                        <option>Mombasa</option>
+                        <option>Kisumu</option>
+                        <option>Eldoret</option>
+                        <option>Nyeri</option>
+                    </select>
+                    <x-input-error :messages="$errors->get('usertype')" class="mt-2" />    
+                </div>
+
+                <!-- Price -->
+            <div class="mt-4">
+                <x-input-label for="price" :value="__('Price in Ksh.')" />
+                <x-text-input id="price" class="block mt-1 w-full" type="number" name="price" :value="old('price')" required />
+                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            </div>
+
+                <!-- Time --> 
+            <div class="mt-4">
+                <x-input-label for="price" :value="__('Starting time')" />
+                <x-text-input id="price" class="block mt-1 w-full" type="time" name="price" :value="old('price')" required />
+                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            </div>
+
+                <button type="submit" class="w-full mt-6 block text-center mx-auto py-3 px-6 bg-black rounded-md text-white hover:opacity-80 focus:opacity-80">
+                   Create New Route
+                </button>
+            </form>
 
 
+
+
+            {{-- You recently added jourmeys --}}
+
+            <div class="my-5 ">
+                <h1 class="py-3 border-b border-orange-600">Your recently added travels</h1>
+    
+                <div class="grid gap-3 mt-4">
+    
+                    @for ($i=0; $i<5; $i++)
+    
+                    <div class="border shadow p-3 rounded-md grid gap-3 grid-cols-3">
+                        <div>
+                            <div class="flex flex-col text-center justify-center h-full w-full bg-gray-200 rounded-md">
+                                <h1 class="text-2xl text-green-600 font-bold">{{ random_int(0, 12) }}</h1>
+                                <div>Booked</div>
+
+                            </div>
+                        </div>
+                        <div class="col-span-2">
+                            <div>Route: Nairobi to Mombasa</div>
+                            <div>Start Time: 10:30 AM</div>
+                            <div>Ticket Price: KES 2,000</div>
+                            <button type="submit" 
+                                class="w-full text-sm block text-center mx-auto py-2 px-2 bg-black rounded-md text-white hover:opacity-80 focus:opacity-80">
+                               View bookings
+                            </button>
+                        </div>
+                    </div>
+                        
+                    @endfor  
+                
+                </div>
 
     </div>
+
+
+
+
+
+
+    
 
 </x-mobile-layout>
--- resources/views/passenger/dashboard.blade.php
+++ resources/views/passenger/dashboard.blade.php
@@ -1,6 +1,6 @@
 <x-mobile-layout header="Passenger">
 
-    <div class="h-screen w-full mt-16 flex flex-col p-4">
+    <div class="h-screen w-full mt-16 pb-6 flex flex-col p-4">
  
         <div class="text-orange-600 text-xl text-center uppercase font-['Righteous']">
             Passenger Dashboard
@@ -43,23 +43,26 @@
 
 
         <div class="mt-5 ">
-            <h1 class="py-3 border-b border-orange-600">Rcently added buses</h1>
+            <h1 class="py-3 border-b border-orange-600">Recently added route travels</h1>
 
             <div class="grid gap-3 mt-4">
 
-                @for ($i=0; $i<10; $i++)
+                @for ($i=0; $i<6; $i++)
 
                 <div class="border shadow p-3 rounded-md grid gap-3 grid-cols-3">
                     <div>
-                        <h1 class="font-bold text-lg">Bus Kenya</h1>
-                        <img src="{{ asset('images/shoolbus.jpg') }}" alt="School Bus" class="h-20 w-20 object-contain">
+                        
+                        <div class="flex flex-col text-center justify-center h-[90%] w-full bg-gray-200 rounded-md">
+                            <i class="fas fa-bus text-orange-600"></i>
+                            <h1 class="font-bold">Bus Kenya</h1> 
+                        </div>
                     </div>
                     <div class="col-span-2">
                         <div>Route: Nairobi to Mombasa</div>
                         <div>Start Time: 10:30 AM</div>
                         <div>Ticket Price: KES 2,000</div>
                         <button type="submit" 
-                            class="w-full text-sm block text-center mx-auto py-2 px-2 bg-black rounded-md text-white hover:opacity-80 focus:opacity-80">
+                            class="w-full text-sm block text-center mx-auto py-2 px-2 bg-black rounded-md text-white hover:bg-orange-600 focus:bg-orange-600">
                            Book Now
                         </button>
                     </div>
